1f89be72255af6f51923cbef172e2da0a7e318d3
driva96 <aboutbklank007@protonmail.com>
2020-07-28 15:29:03 +0200

    Logo and basic map

--- Case_Study_App_Group_50.R
+++ Case_Study_App_Group_50.R
@@ -14,19 +14,20 @@ if(!require("install.load")){
 }
 library(install.load)
 
-install_load("shiny", "leaflet", "htmltools", "highcharter","ggplot2", "maps","dplyr","tidyverse","rvest","raster","sf","rgeos","plotly")
+install_load("shiny", "leaflet", "htmltools", "highcharter","ggplot2", "maps","dplyr","tidyverse","rvest","raster","sf","rgeos","plotly","jpeg","png")
 
 #test 
 test <- final_data_Group_50%>%
   head(50)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    #test for map
-    leafletOutput("map"),
     # Application title
     titlePanel("Placeholder"),
-    
+    #test for map
+    leafletOutput("map"),
+    #display logo
+    uiOutput("Logo")
+
     # Sidebar with a slider input for number of bins 
     #sidebarLayout(
     # sidebarPanel(
@@ -46,6 +47,13 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    #image from URL https://stackoverflow.com/questions/45709189/renderimage-from-url-and-clickable
+    output$Logo <- renderUI({
+        # Return a list containining the filename
+        imgur1 <- "https://www.qw.tu-berlin.de/fileadmin/_processed_/8/8d/csm_QW_ohne_Text_print_a4670877cd.jpg"
+        tags$img(src = imgur1, width=80, height=65)    
+    })
+    
     output$map <- renderLeaflet({
         leaflet(test)%>%
         addTiles()%>%
