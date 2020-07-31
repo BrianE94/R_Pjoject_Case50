511d0fa33507a6f3003e9fea9013458b75e0f660
BrianE94 <68274848+BrianE94@users.noreply.github.com>
2020-07-31 17:38:12 +0200

    1. Versuch Variabler Koeffizient

--- Case_Study_App_Group_50.R
+++ Case_Study_App_Group_50.R
@@ -16,16 +16,18 @@ library(install.load)
 
 install_load("shiny", "leaflet", "htmltools", "highcharter","ggplot2", "maps","dplyr","tidyverse","rvest","raster","sf","rgeos","plotly","jpeg","png")
 
-#load saved dataframe 
+#load saved dataframe from Case_Study_Group_50.Rmd
 load(file="final_data_Group_50.Rda")
 
-#test 
+#test: First run with 1000  
 test <- final_data_Group_50%>%
-  head(1000)
+  head(10000)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     # Application title
     titlePanel("Placeholder"),
+    # Radius Input
+    sliderInput("value", "Selected Radius in km:", min = 25, max= 700,value = 50,step=25),
     #test for map
     leafletOutput("map"),
     #display renderPlot
@@ -33,21 +35,6 @@ ui <- fluidPage(
     #display logo
     uiOutput("Logo")
 
-    # Sidebar with a slider input for number of bins 
-    #sidebarLayout(
-    # sidebarPanel(
-    #    sliderInput("bins",
-    #               "Number of bins:",
-    #               min = 1,
-    #               max = 50,
-    #              value = 30)
-    #),
-    
-    # Show a plot of the generated distribution
-    #mainPanel(
-    
-    #)
-    #)
 )
 
 # Define server logic required to draw a histogram
@@ -58,6 +45,15 @@ server <- function(input, output) {
         imgur1 <- "https://www.qw.tu-berlin.de/fileadmin/_processed_/8/8d/csm_QW_ohne_Text_print_a4670877cd.jpg"
         tags$img(src = imgur1, width=80, height=65)    
     })
+    # Make a reactve Radius
+      distanz <- reactive({
+        test
+        if(!is.null(input$value)){
+          test <-test%>%
+            filter(test$dist <= input$value[1]*1000)
+        }
+        test
+      })
     #map output 
     output$map <- renderLeaflet({
       #golden marker for hamburg
@@ -67,7 +63,10 @@ server <- function(input, output) {
         iconWidth = 25, iconHeight = 41,
         iconAnchorX = 25/2, iconAnchorY = 41
       )
+      
       #draw the map and add markers
+      # See: https://stackoverflow.com/questions/40861908/shiny-r-implement-slider-input
+      test <- distanz()
         leaflet(test)%>%
         addTiles()%>%
         addMarkers(lng=~Laengengrad, 
@@ -80,13 +79,16 @@ server <- function(input, output) {
                                 (Produktionsdatum))
                    )%>%
         addMarkers(lng=9.993682, lat=53.551085, icon=hamburg_marker)%>%
-        addCircles(lng=9.993682, lat=53.551085,radius = 10000)
+        addCircles(lng=9.993682, lat=53.551085,radius = input$value[1]*1000)
+        
     })
+    
     #barplot output
     output$barplot <- renderPlot({
       ggplot(test)+
         geom_histogram(aes(Bundesland), stat="count")
     })
+    # Connect slider bar with
 }
 
 # Run the application 
